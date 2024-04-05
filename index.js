405ab935b608c142505db006c54419474a7a73cd
Ton3t <elton3t@gmail.com>
2024-04-05 13:05:03 +0200

    conexion a mongodb

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ app.use(express.json());
 app.listen(5000, () => console.log("Servidor corriendo en el puerto 5000"));
 
 app.use("/posts", require("./routers/postRouter"));
+
+// CONECTAR A MONGODB
+
+mongoose
+  .connect(
+    "mongodb+srv://tonet:aDgAH4BavS5YGKCO@snipet-manager.dq5intb.mongodb.net/?retryWrites=true&w=majority&appName=Snipet-manager"
+  )
+  .then(console.log("Conectado a MONGODB"));
