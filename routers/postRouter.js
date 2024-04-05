2bb69508f405fae117d67cc03b40c43d7fd03d7d
Ton3t <elton3t@gmail.com>
2024-04-05 13:08:46 +0200

    validando datos de los post recibidos

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -4,19 +4,32 @@ const Post = require("../models/postModel");
 // recibir posts
 
 router.get("/", (req, res) => {
-    res.send("Post test");
+  res.send("Post test");
 });
 
 // enviar posts
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
+  try {
     const { titulo, descripcion, code } = req.body;
 
+    if (!titulo && !descripcion && !code) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Debe rellenar todos los campos." });
+    }
+
     const newPost = new Post({
-        titulo, descripcion, code
+      titulo,
+      descripcion,
+      code,
     });
 
-    newPost.save();
+    const savedPost = await newPost.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
