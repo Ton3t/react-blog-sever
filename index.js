9a23e28944ea69fda53713bb404c0a33b5fbebc4
Ton3t <elton3t@gmail.com>
2024-04-05 12:49:28 +0200

    api leer json

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, () => console.log("Servidor corriendo en el puerto 5000"));
 
 app.use("/posts", require("./routers/postRouter"));
